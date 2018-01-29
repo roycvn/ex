9ce33269bdb3164b760b7b0136427b279ce6ec10
roy <me@roy.com>
2018-01-29 11:40:53 +0530

    New Files

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var https = require('http');
+
+fs.writeFile("index.html", "<div>Creating index file. Script Executed</div>", function(error){
+    if(error){
+        console.log(error);
+    } else {
+        console.log('Script Execueted');
+    }
+});
+
+var fileurl = "http://fontawesome.io/assets/img/logo-themeisle.png";
+https.get(fileurl, function(response){
+    console.log(response);
+    if(response){
+        fs.writeFile("image.txt", response, function(error){
+            if(error){ 
+                console.log('File Creation Error ' + error);
+            } else {
+                console.log('File Created');            
+            }
+        });
+    }
+});
+
